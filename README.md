f33c3aac10924a2c5f42d81a3ba15b3037c703b2
Gromovoi Stanislav <gromila12@rambler.ru>
2022-12-06 14:53:04 +0600

    start project

---
+++ README.md
@@ -0,0 +1,1 @@
+## NestJS + PostgreSQL with TypeORM
---
+++ src/tag/tag.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
+
+@Injectable()
+export class TagService {
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
+  }
+
+  async create(dto: { name: string }) {
+    const tag = this.tagRepository.create(dto);
+    return this.tagRepository.save(tag);
+  }
+}

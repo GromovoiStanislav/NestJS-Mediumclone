56f55e63fba285dc5a0a57cf128f705e5fcbae09
Gromovoi Stanislav <stasgromovoi@gmail.com>
2022-12-22 23:02:43 +0000

    updating-current-user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import {
     Body,
     Controller,
     Get,
-    Post, UseGuards,
+    Post, Put, UseGuards,
     UsePipes,
     ValidationPipe,
 } from '@nestjs/common';
@@ -13,6 +13,7 @@ import {LoginUserDto} from './dto/loginUser.dto';
 import {User} from './decorators/user.decorator';
 import {UserEntity} from './user.entity';
 import {AuthGuard} from "./guards/auth.guard";
+import {UpdateUserDto} from "./dto/updateUser.dto";
 
 @Controller()
 export class UserController {
@@ -43,5 +44,19 @@ export class UserController {
     async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
         return this.userService.buildUserResponse(user);
     }
+
+    @Put('user')
+    @UseGuards(AuthGuard)
+    async updateCurrentUser(
+        @User('id') currentUserId: number,
+        @Body('user') updateUserDto: UpdateUserDto,
+    ): Promise<UserResponseInterface> {
+        const user = await this.userService.updateUser(
+            currentUserId,
+            updateUserDto,
+        );
+        return this.userService.buildUserResponse(user);
+    }
+
 
 }

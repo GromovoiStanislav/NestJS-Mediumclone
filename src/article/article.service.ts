ec05eccb01e081ffecb5c1f63bdcc03cee43e372
Gromovoi Stanislav <stasgromovoi@gmail.com>
2022-12-24 16:03:21 +0000

    current-user-feed

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -8,12 +8,14 @@ import {ArticleResponseInterface} from "./types/articleResponse.interface";
 import slugify from 'slugify';
 import {ArticlesResponseInterface} from "./types/articlesResponse.interface";
 import dataSource from "../data-source";
+import {FollowEntity} from "../profile/follow.entity";
 
 @Injectable()
 export class ArticleService {
     constructor(
         @InjectRepository(ArticleEntity) private readonly articleRepository: Repository<ArticleEntity>,
         @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+        @InjectRepository(FollowEntity) private readonly followRepository: Repository<FollowEntity>,
     ) {
     }
 
@@ -76,6 +78,40 @@ export class ArticleService {
 
         return { articles: articlesWithFavorited, articlesCount };
     }
+
+
+    async getFeed(currentUserId: number, query: any): Promise<ArticlesResponseInterface> {
+        const follows = await this.followRepository.find({where:{followerId: currentUserId}});
+
+        if (follows.length === 0) {
+            return { articles: [], articlesCount: 0 };
+        }
+
+        const followingUserIds = follows.map((follow) => follow.followingId);
+
+        const queryBuilder = getRepository(ArticleEntity)
+            .createQueryBuilder('articles')
+            .leftJoinAndSelect('articles.author', 'author')
+            .where('articles.authorId IN (:...ids)', { ids: followingUserIds });
+
+        queryBuilder.orderBy('articles.createdAt', 'DESC');
+
+        const articlesCount = await queryBuilder.getCount();
+
+        if (query.limit) {
+            queryBuilder.limit(query.limit);
+        }
+
+        if (query.offset) {
+            queryBuilder.offset(query.offset);
+        }
+
+        const articles = await queryBuilder.getMany();
+
+        return { articles, articlesCount };
+    }
+
+
 
 
     async createArticle(currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<ArticleEntity> {
